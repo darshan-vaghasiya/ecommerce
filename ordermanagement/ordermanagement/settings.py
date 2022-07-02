eb32d443ac9f2ab16be0da41908a1640e05f24d8
darshanv <hlink@1234>
2022-07-02 16:04:11 +0530

    remove email cred

--- ordermanagement/ordermanagement/settings.py
+++ ordermanagement/ordermanagement/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'darshanv.hyperlinkinfosystem@gmail.com'
-EMAIL_HOST_PASSWORD = 'darshan@7799'
+EMAIL_HOST_USER = '*************************'
+EMAIL_HOST_PASSWORD = '***********'
 
 # Adding pagination
 REST_FRAMEWORK = {
